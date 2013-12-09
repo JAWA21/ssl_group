e7bf035ea63a920dc0c59fb830b7a7052d5c1f3f
Jeanna <jawa@fullsail.edu>
2013-12-09 18:28:13 -0500

    working on registerModel and loginModel

--- models/loginModel.php
+++ models/loginModel.php
@@ -4,14 +4,27 @@
 session_start();
 
 //user login
-$dbh = new PDO("mysql: hostname=localhost; dbname=simpBlog; port=8889", "root", "root");
-$stmt = $dbh->prepare("SELECT user_id users(user_name, user_pass) VALUES (:user_name, :user_pass)");
+
+
+	$username =$_POST["username"];
+	$pass = $_POST["password"];
+
+	echo $result;
 
 class loginModel {
 	//The view() function will display a page in the views subdirectory
 
 	public function checklogin($username, $password) {
 		//check user/pass against db - returns true on success
+		// if($username == $result:user_name){
+		$dbh = new PDO("mysql: hostname=localhost; dbname=simpBlog; port=8889", "root", "root");
+$result = mysql_query("SELECT user_name,user_pass FROM users where user_id=$_SESSION['userid'] VALUES (:user_name, :user_pass)");
+
+
+	$result->bindParam(':user_name', $username);
+	$result->bindParam(':user_pass', $password);
+
+		// }
 		$_SESSION['userid']=1;
 		return false;
 	}
