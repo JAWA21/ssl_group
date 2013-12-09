98d0020bc8f57ae2ae3a5d4a8a9387d1adb29ba6
Jeanna <jawa@fullsail.edu>
2013-12-09 18:05:04 -0500

    added

--- controllers/viewController.php
+++ controllers/viewController.php
@@ -5,3 +5,8 @@
  * Date: 12/9/13
  * Time: 5:22 PM
  */ 
+
+require_once('models/viewModel.php');
+
+$view = new viewModel(); 
+$view->getView(strtolower($queryMethod));
--- models/loginModel.php
+++ models/loginModel.php
@@ -1,5 +1,25 @@
 <?php
 //loginModel.php
 
+session_start();
+
 //user login
+$dbh = new PDO("mysql: hostname=localhost; dbname=simpBlog; port=8889", "root", "root");
+$stmt = $dbh->prepare("SELECT user_id users(user_name, user_pass) VALUES (:user_name, :user_pass)");
+
+class loginModel {
+	//The view() function will display a page in the views subdirectory
+
+	public function checklogin($username, $password) {
+		//check user/pass against db - returns true on success
+		$_SESSION['userid']=1;
+		return false;
+	}
+
+	public function signout() {
+		//clear out the session information
+		session_destroy();
+		session_start();
+	}
+}
 ?>
