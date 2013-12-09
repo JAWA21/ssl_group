72f8917f2b0c6f9fe57f9cf6a066dd6201b28728
Jeanna <jawa@fullsail.edu>
2013-12-09 17:54:12 -0500

    setting up index.php

--- index.php
+++ index.php
@@ -5,3 +5,32 @@
  * Date: 12/9/13
  * Time: 5:23 PM
  */ 
+
+//index.php - default routing page
+
+//set Action, Method, and Data passed through querystring
+if (isset($_GET['action'])) {
+	$queryAction=trim($_GET['action']);
+} else {
+	//an ACTION has not been returned (root of site)
+	//set action to home page
+	$queryAction="view";
+	$queryMethod="home";
+}
+if (isset($_GET['method'])) {
+	$queryMethod=trim($_GET['method']);
+} else {
+	if (!isset($queryMethod)){ $queryMethod=""; } 
+}
+if (isset($_GET['data'])) {
+	$queryData=trim($_GET['data']);
+} else {
+	$queryData="";
+}
+
+if (file_exists("controllers/".$queryAction."Controller.php")) {
+	include("controllers/".$queryAction."Controller.php");
+} else {
+	//controller page does not exist, return 404
+	include('404.php');
+}
