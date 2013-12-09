888efdd0e99bb0c890247fcfadedd1184b3543e3
Matt Antonio <mantonio@fullsail.edu>
2013-12-09 17:49:48 -0500

    adding css

--- views/homeView.php
+++ views/homeView.php
@@ -6,33 +6,39 @@
 	<body>
 
 		<header>
-			<h1 id="logo">SSL1312 Blog</h1>
-			<nav>
-				<ul>
-					<li><a href="?action=view&method=register">Register</a></li>
-					<li><a href="?action=view&method=blog">Blog</a></li>
-					<li><a href="?action=login">Login</a></li>
-				</ul>
-			</nav>
+
+			<div class="wrap">
+				<h1 id="logo">SSL1312 Blog</h1>
+				<nav>
+					<ul>
+						<li><a href="?action=view&method=register">Register</a></li>
+						<li><a href="?action=view&method=blog">Blog</a></li>
+						<li><a href="?action=login">Login</a></li>
+					</ul>
+				</nav>
+			</div>
+
 		</header>
 
-		<img id="blogImg" src="images/blog.jpg" alt="SSL1312 Blog">
+		<div class="wrap">
+			<img id="blogImg" src="images/blog.jpg" alt="SSL1312 Blog">
 
-		<aside id="loginForm">
-			<form method="POST" action="?action=login">
-				<p>
-					<label for="username">Username:</label>
-					<input type="text" name="username" id="username">
-				</p>
+			<aside id="loginForm">
+				<form method="POST" action="?action=login">
+					<p>
+						<label for="username">Username:</label>
+						<input type="text" name="username" id="username">
+					</p>
 
-				<p>
-					<label for="password">Password:</label>
-					<input type="password" name="userpass" id="userpass">
-				</p>
+					<p>
+						<label for="password">Password:</label>
+						<input type="password" name="userpass" id="userpass">
+					</p>
 
-				<input type="submit" value="Submit" id="loginBtn" name="loginBtn">
-			</form>
-		</aside>
+					<input type="submit" value="Submit" id="loginBtn" name="loginBtn">
+				</form>
+			</aside>
+		</div>
 
 	</body>
 </html>
