07d0a9a9fd0460b1862752279d23397002b883b0
Jeanna <jawa@fullsail.edu>
2013-12-09 19:53:02 -0500

    trying to get the register working

--- models/registerModel.php
+++ models/registerModel.php
@@ -5,37 +5,28 @@
 	session_start();
 
 	class registerModel {
-		function loggedin($data){
+		function loggedin($username, $pass, $confirm){
 			//connect to database
-			$dbh = new PDO("mysql: hostname=localhost; dbname=simpBlog; port=8889", "root", "root");
+			$dbh = new PDO("mysql: hostname=localhost; dbname=simpleBlog; port=8889", "root", "root");
 			$stmt = $dbh->prepare("INSERT INTO users(user_name, user_pass) VALUES (:user_name, :user_pass)");
 
-			$stmt->bindParam(':user_name', $name);
+			$str = $pass;
+			$salt = 'mysectretpassword';
+			$confirm = md5($salt.$str);
+			$hashsalt = md5($salt.$str);
+
+			$stmt->bindParam(':user_name', $username);
 			$stmt->bindParam(':user_pass', $hashsalt);
-
-	$name =$_POST["regName"];
-	$str = $_POST["regPass"];
-
-
-
-			$text = $_SESSION['text'];
-
-			if(isset($_POST["pass"]) && !empty($_POST["pass"])){
-				$salt = 'mysectretpassword';
-				$hashsalt = md5($salt.$str);
-			}
-
-  	if(!empty($_POST['regName']) && !empty($_POST['regPass']) && !empty($_POST['regConfirm'])) {  
-		$confirm = $_POST["regConfirm"];
-		
-		if($confirm == $str){
+			
+			
+		if($confirm == $hashsalt){
 			$err = 'no error';
 			$stmt->execute();
 		}else{
 			$err = 'error';
 		}
 
-		echo "Input: ".$capt."<br/>";
+		
 	}else{
 		echo "all fields are required.";
 	}
