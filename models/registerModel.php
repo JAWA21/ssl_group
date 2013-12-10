70b9accbaa46a800232523dc771f3d96669902e1
Matt Antonio <mantonio@fullsail.edu>
2013-12-09 23:55:01 -0500

    registration

--- models/registerModel.php
+++ models/registerModel.php
@@ -7,17 +7,9 @@
 	class registerModel {
 		function loggedin($data){
 			//connect to database
-			$dbh = new PDO("mysql: hostname=localhost; dbname=simpBlog; port=8889", "root", "root");
-			$stmt = $dbh->prepare("INSERT INTO users(user_name, user_pass) VALUES (:user_name, :user_pass)");
-
-			$stmt->bindParam(':user_name', $name);
-			$stmt->bindParam(':user_pass', $hashsalt);
-
-	$name =$_POST["regName"];
-	$str = $_POST["regPass"];
-
 
 			$name = $_POST["regName"];
+			$str = $_POST["regPass"];
 
 			$text = $_SESSION['text'];
 
@@ -26,19 +18,26 @@
 				$hashsalt = md5($salt.$str);
 			}
 
-  	if(!empty($_POST['regName']) && !empty($_POST['regPass']) && !empty($_POST['regConfirm'])) {  
-		$confirm = $_POST["regConfirm"];
-		
-		if($confirm == $str){
-			$err = 'no error';
-			$stmt->execute();
-		}else{
-			$err = 'error';
+		    if(!empty($_POST['regName']) && !empty($_POST['regPass']) && !empty($_POST['regConfirm'])) {
+			    $dbh = new PDO("mysql: hostname=localhost; dbname=simpleBlog; port=8889", "root", "root");
+			    $stmt = $dbh->prepare("INSERT INTO users(user_name, user_pass) VALUES (:user_name, :user_pass)");
+
+			    $stmt->bindParam(':user_name', $name);
+			    $stmt->bindParam(':user_pass', $hashsalt);
+
+				$confirm = $_POST["regConfirm"];
+
+				if($confirm == $str){
+					$err = 'no error';
+					$stmt->execute();
+				}else{
+					$err = 'error';
+				}
+
+				echo "Input: ".$capt."<br/>";
+			}else{
+				echo "all fields are required.";
+			}
 		}
-
-		echo "Input: ".$capt."<br/>";
-	}else{
-		echo "all fields are required.";
 	}
-
 ?>
