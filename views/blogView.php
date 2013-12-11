7333696a37ec4c3f20292defd629df63800297f8
Matt Antonio <mantonio@fullsail.edu>
2013-12-11 11:20:21 -0500

    adding css to blogView

--- views/blogView.php
+++ views/blogView.php
@@ -1,15 +1,19 @@
+<div class="wrap">
+	<form id="blogPost" name="blogPost" method="post" action="?method=views&action=blog">
 
-<form id="blogPost" name="blogPost" method="post" action="?method=views&action=blog">
+		<input type="hidden" id="blogid" name="blogid" />
 
-	<input type="hidden" id="blogId" name="blogId" />
+		<div class="blogToolbar"></div>
+		<p>
+			<label for="title">Title:</label>
+			<input type="text" id="title" name="title" />
+		</p>
 
-	<div class="blogToolbar"></div>
+		<p>
+			<label for="text">Text:</label>
+			<textarea id="text" rows="6" cols="50"></textarea>
+		</p>
 
-	<label for="title">Title:</label>
-	<input type="text" id="title" name="title" />
-
-	<label for="text">Text:</label>
-	<textarea id="text" rows="4" cols="50"></textarea>
-
-	<input type="submit" value="Post" id="sendBtn" />
-</form>
+		<input type="submit" value="Post" id="postBtn" />
+	</form>
+</div>
